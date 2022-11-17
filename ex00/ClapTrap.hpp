4eb09586c3f26cf2a8bbf7f57211fcc4dae9a395
aweaver <aweaver@student.42.fr>
2022-11-17 17:32:21 +0100

    ADD EX00:
    Adding files for ex00.
    Now it begins for the real deal... inheritence in the next exercise :D

---
+++ ex00/ClapTrap.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/17 16:02:42 by aweaver           #+#    #+#             */
+/*   Updated: 2022/11/17 17:19:00 by aweaver          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+# include <iostream>
+
+class ClapTrap
+{
+	public:
+		ClapTrap(void);
+		~ClapTrap(void);
+		ClapTrap(std::string name);
+		ClapTrap(ClapTrap const& source);
+		ClapTrap & operator=(ClapTrap const& rhs);
+		void	attack(const std::string &target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+	protected:
+	private:
+		std::string	_name;
+		unsigned int	_hp;
+		unsigned int	_mana;
+		unsigned int	_dmg;
+};
+
+#endif
